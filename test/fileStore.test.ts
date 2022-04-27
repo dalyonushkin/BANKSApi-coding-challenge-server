be8a2d341e237101ee03418d3431ff6ffdc140d4
Denis Alyonushkin <denis.alyonushkin@gmail.com>
2022-04-27 16:13:40 +0300

    test

--- test/fileStore.test.ts
+++ test/fileStore.test.ts
@@ -39,10 +39,29 @@ describe("File Store", () => {
             expect(transfers).toEqual(filecontent);
         });
         it("when open non existing file should return empty oblect", () => {
-
             const transfers = readFileStore(fileStorePath);
             expect(transfers).toEqual({});
         });
+        it("when read a content is same as a content after write", () => {
+            const filecontent: TransfersList = {
+                a: { amount: 50, date: "2022-01-02", iban: "1232" },
+                b: { amount: 50.22, date: "2022-01-02", iban: "1232", accountHolder: "a b", note: "note" }
+            };
+            const filecontent2: TransfersList = {
+                a1: { amount: 501, date: "2022-01-02", iban: "1232" },
+                b2: { amount: 501.22, date: "2022-01-02", iban: "1232", accountHolder: "a b", note: "note" }
+            };
+            writeFileStore(fileStorePath, filecontent);
+            const transfers = readFileStore(fileStorePath);
+            expect(transfers).toEqual(filecontent);
+            const transfers2 = readFileStore(fileStorePath);
+            expect(transfers2).toEqual(filecontent);            
+            writeFileStore(fileStorePath, filecontent2);
+            const transfers3 = readFileStore(fileStorePath);
+            expect(transfers3).toEqual(filecontent2);            
+            const transfers4 = readFileStore(fileStorePath);
+            expect(transfers4).toEqual(filecontent2);        
+        });        
     });
     describe("when adding Transfer record ", () => {
         it("should add new record to store", () => {
@@ -69,21 +88,21 @@ describe("File Store", () => {
             expect(() => { store$addTransfer(fileStorePath, "1a", transferRec); }).toThrow(TransferAlreadyExistsInStoreError);
         });
     });
-        
-        it("should delete Transfer record", () => {
-            const transferRec: Transfer = {
-                amount: 50,
-                date: "2022-01-22",
-                iban: "DE1232",
-                accountHolder: "a b",
-                note: "note"
-            };
-            store$addTransfer(fileStorePath, "1a", transferRec);
-            store$addTransfer(fileStorePath, "1b", transferRec);
-            expect(readFileStore(fileStorePath)).toEqual({ "1a": transferRec, "1b": transferRec });
-            store$deleteTransfer(fileStorePath, "1a");
-            expect(readFileStore(fileStorePath)).toEqual({ "1b": transferRec});
-        });
+
+    it("should delete Transfer record", () => {
+        const transferRec: Transfer = {
+            amount: 50,
+            date: "2022-01-22",
+            iban: "DE1232",
+            accountHolder: "a b",
+            note: "note"
+        };
+        store$addTransfer(fileStorePath, "1a", transferRec);
+        store$addTransfer(fileStorePath, "1b", transferRec);
+        expect(readFileStore(fileStorePath)).toEqual({ "1a": transferRec, "1b": transferRec });
+        store$deleteTransfer(fileStorePath, "1a");
+        expect(readFileStore(fileStorePath)).toEqual({ "1b": transferRec });
+    });
     describe("when update Transfer record ", () => {
         it("should update existing Transfer record", () => {
             const transferRec: Transfer = {
@@ -111,7 +130,7 @@ describe("File Store", () => {
                 iban: "DE1232"
             };
             store$addTransfer(fileStorePath, "1a", transferRec2);
-            expect(readFileStore(fileStorePath)).toEqual({ "1a": transferRec2});
+            expect(readFileStore(fileStorePath)).toEqual({ "1a": transferRec2 });
             expect(() => { store$updateTransfer(fileStorePath, "1b", transferRec2); }).toThrow(TransferNotFoundInStoreError);
         });
     });
